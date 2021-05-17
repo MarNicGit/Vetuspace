bf36c102d4ca824c21f5488a12d9fd894888e32b
MarNicGit <47538428+MarNicGit@users.noreply.github.com>
2021-05-17 21:01:31 +0200

    basic icon selecting and deselecting

--- www/scripts/elements/desktop.ts
+++ www/scripts/elements/desktop.ts
@@ -1,5 +1,24 @@
+import { DesktopIcon } from "./desktopIcon";
+
 export class Desktop extends HTMLElement {
     constructor(){
         super();
+    }
+
+    connectedCallback(){
+        this.registerListeners();
+    }
+
+    registerListeners(){
+        this.addEventListener('click', this.onClick)
+    }
+
+    onClick(e: MouseEvent){
+        e.stopPropagation();
+        let icons = document.getElementsByTagName('desktop-icon');
+        for (let index = 0; index < icons.length; index++) {
+            const icon = icons[index] as DesktopIcon;
+            icon.unselect();
+        }
     }
 }
--- www/scripts/elements/desktopIcon.ts
+++ www/scripts/elements/desktopIcon.ts
@@ -5,7 +5,31 @@ export class DesktopIcon extends HTMLElement {
 
     connectedCallback(){
         console.log('element added!');
+        this.registerListeners();
         this.render();
+    }
+
+    registerListeners(){
+        this.addEventListener('dblclick', this.onDoubleClick)
+        this.addEventListener('click', this.onClick)
+    }
+
+    onClick(e: MouseEvent){
+        e.stopPropagation();
+        this.select()
+    }
+
+    onDoubleClick(e: MouseEvent){
+        console.log(e);
+        this.unselect();
+    }
+
+    select(){
+        this.classList.add('selected');
+    }
+
+    unselect(){
+        this.classList.remove('selected');
     }
 
     render(){
@@ -15,9 +39,13 @@ export class DesktopIcon extends HTMLElement {
         let icon = document.createElement('img' )
         icon.src = iconUrl;
 
+        let iconWrapper = document.createElement('div');
+        iconWrapper.className = "wrapper";
+        iconWrapper.innerHTML = icon.outerHTML;
+
         let label = document.createElement('label');
         label.innerText = title;
 
-        this.innerHTML = icon.outerHTML + label.outerHTML;
+        this.innerHTML = iconWrapper.outerHTML + label.outerHTML;
     }
 }
