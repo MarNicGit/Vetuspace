2b5f5a3f811e66c7042162ceb0d3aee52242a005
MarNicGit <47538428+MarNicGit@users.noreply.github.com>
2023-05-29 23:13:44 +0200

    added desktop component (empty)

--- Site/src/app/app-routing.module.ts
+++ Site/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DesktopComponent } from './desktop/desktop.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: DesktopComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
