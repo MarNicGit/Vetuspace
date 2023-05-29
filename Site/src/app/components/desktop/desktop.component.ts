900187e0aae8b13893898678b8cfb148868a79e5
MarNicGit <47538428+MarNicGit@users.noreply.github.com>
2023-05-30 00:30:52 +0200

    desktop adjustments, test icon

--- Site/src/app/components/desktop/desktop.component.ts
+++ Site/src/app/components/desktop/desktop.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IconModel } from '../icon/icon.component';
 
 @Component({
   selector: 'app-desktop',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./desktop.component.scss']
 })
 export class DesktopComponent {
-
+  foobar: IconModel = {
+    label: 'Hello world',
+    alt: 'alt text!!',
+    src: 'https://placehold.co/64x64.png'
+  }
 }
